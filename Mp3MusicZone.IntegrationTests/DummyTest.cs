97521c7caa23965adeb701a99d72a5709908ca09
vncpetrov <zerakikenpachi4@gmail.com>
2018-06-24 19:31:49 +0300

    Fixed failing build

--- Mp3MusicZone.IntegrationTests/DummyTest.cs
+++ Mp3MusicZone.IntegrationTests/DummyTest.cs
@@ -1,11 +1,7 @@
-﻿using Mp3MusicZone.Auth;
-using Mp3MusicZone.EfDataAccess;
+﻿using Mp3MusicZone.EfDataAccess;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mp3MusicZone.IntegrationTests
 {
@@ -19,14 +15,6 @@ namespace Mp3MusicZone.IntegrationTests
             bool any = context.Users.Any();
 
             Assert.IsFalse(any);
-            EmailSenderService sender = new EmailSenderService(TestsInitializer.EmailSettings);
-
-            Task.Run(async () =>
-            {
-                await sender.SendEmailAsync("b2h.klo@abv.bg", "Testing Integration Testing", "Thats for the tests");
-            })
-            .GetAwaiter()
-            .GetResult();
         }
     }
 }
