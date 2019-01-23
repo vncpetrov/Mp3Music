3c45b478a9a7d2c121c97d5dbd5db8c82b11c287
vncpetrov <zerakikenpachi4@gmail.com>
2019-01-23 14:42:07 +0200

    Fixing failing build

---
+++ Mp3MusicZone.EfDataAccess/Migrations/20190122204131_UrlColumnRenamedToAdditionalInfo.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mp3MusicZone.EfDataAccess.Migrations
+{
+    public partial class UrlColumnRenamedToAdditionalInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "UnhandledExceptionEntries",
+                newName: "AdditionalInfo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AdditionalInfo",
+                table: "UnhandledExceptionEntries",
+                newName: "Url");
+        }
+    }
+}
